75841c4b6c3c0fd275696c79573a00107c92457c
Rafael Silva Willians <rafael.willians@outlook.com>
2025-10-21 22:59:32 +0000

    Adicionado teste_aula2

---
+++ python/teste_aula2.py
@@ -0,0 +1,11 @@
+nota1 = 6
+nota2 = 7
+nota3 = 10
+nota4 = 3
+media = (nota1+nota2+nota3+nota4)/4
+# metade_media = ((nota1+nota2+nota3+nota4)/4)/2
+metade_media = media/2
+
+print("Média = ", media)
+# print("Metade da média = ", metade_media)
+print("MeiaMedia =", media/2)
